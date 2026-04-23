79bdee343c2730645571bbb984c0b470ecb7d439
Jochen Mader <jochen.mader@hivemq.com>
2026-04-23 06:54:37 +0200

    Switch to Java 25

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,7 +67,7 @@ dependencies {
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
+        languageVersion.set(JavaLanguageVersion.of(25))
     }
     withJavadocJar()
     withSourcesJar()
